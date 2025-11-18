aa99e872509ffbbec4bc507d38a0eadfe44eac5a
alexmortelsison <alexmortelsison@gmail.com>
2025-11-18 23:05:33 +0900

    updated homepage

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { AiOutlineShopping } from "react-icons/ai";
+
+export default function Navbar() {
+  return (
+    <div className="absolute z-20 h-[60] w-screen items-center flex justify-center text-center text-white pt-20">
+      <div className="flex justify-between items-center space-x-46">
+        <div>
+          <Link href={"/shop"}>SHOP</Link>
+        </div>
+        <div>
+          <Link href={"/shop"}>STORY</Link>
+        </div>
+        <div className="px-32">
+          <Link href={"/"}>
+            <Image
+              src={"/logo.png"}
+              alt={""}
+              width={500}
+              height={500}
+              className="object-cover w-[150]"
+            />
+          </Link>
+        </div>
+        <div>
+          <Link href={"/shop"}>LOGIN</Link>
+        </div>
+        <div className="relative">
+          <AiOutlineShopping className="text-2xl" />
+          <div className="absolute -top-1 -right-3 text-white text-sm border bg-red-500 rounded-full px-1.5">
+            <p>1</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{
+}
---
+++ app/components/VideoPlayer.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRef, useState } from "react";
+
+export default function VideoPlayer() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isMuted, setIsMuted] = useState(true);
+
+  const handleUnmute = () => {
+    if (videoRef.current) {
+      videoRef.current.muted = false;
+      setIsMuted(false);
+    }
+  };
+
+  return (
+    <div className="relative inset-0" onClick={handleUnmute}>
+      <div className="bg-linear-to-b from-black/90 via-black/20 to-black/90 absolute inset-0"></div>
+      <div className="">
+        <video
+          ref={videoRef}
+          src={"/final.mp4"}
+          autoPlay
+          muted={isMuted}
+          playsInline
+          loop
+        />
+      </div>
+    </div>
+  );
+}
